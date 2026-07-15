cdc9c42393050b1a3410e6cf6590fd67d20849f0
Nuwani Chandrasiri <nuwanichandrasiri@gmail.com>
2026-07-15 11:00:12 +0530

    Update database connection details in db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "sqlXXX.infinityfree.com";
-$dbname = "epiz_XXXXXXX_eventhive";
-$username = "epiz_XXXXXXX";
-$password = "your_generated_password";
+$host = "sql212.infinityfree.com";
+$dbname = "if0_42413935_epiz_eventhive";
+$username = "if0_42413935";
+$password = "Ma9B6Qim1S";
 
 // Connect to MySQL database
 $conn = new mysqli($host, $username, $password, $dbname);
